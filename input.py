db989fc976490b3f890453ed4b553de7f51ff187
carlosalme <31869874+carlosalme@users.noreply.github.com>
2021-06-29 19:54:50 -0500

    Add files via upload

---
+++ input.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2.7
+
+name = input("What is your name? ")
+birthdate = input("What is your bithdate? ")
+age = int(input("How old are you? "))
+
+print(f"{name} was born on {birthdate}")
+print(f"Half of your age is {age / 2}")
